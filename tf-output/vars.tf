26eff46d87f6db1ddab1ca84bb388e0be4cc8444
sripathi <kammari.sripathi@gmail.com>
2022-10-10 15:24:38 +0530

    Create vars.tf

---
+++ tf-output/vars.tf
@@ -0,0 +1,18 @@
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+  default = "us-ashburn-1"
+}
+
+variable "compartment_id" {
+}
