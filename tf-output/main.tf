01bcbaa56a8437880cf931c5f9daf5f2af1dd17b
sripathi <kammari.sripathi@gmail.com>
2022-10-10 12:35:46 +0000

    test commit

--- tf-output/main.tf
+++ tf-output/main.tf
@@ -23,6 +23,30 @@ resource "oci_monitoring_alarm" "OIT_DEV_COMPUTES_CPU_CRITICAL_1211" {
   }
 }
 
+resource "oci_monitoring_alarm" "OIT_DEV_COMPUTES_CPU_CRITICAL_12113" {
+  compartment_id        = var.compartment_id
+  destinations          = [oci_ons_notification_topic.oci_topic.id]
+  display_name          = "OIT_DEV_COMPUTES_CPU_CRITICAL_12113"
+  is_enabled            = true
+  metric_compartment_id = var.compartment_id
+  namespace             = "oci_computeagent"
+  query                 = "CpuUtilization[5m].mean() > 95"
+  severity              = "WARNING"
+
+  body                             = "Critical - High CPU utilization reached"
+  message_format                   = "ONS_OPTIMIZED"
+  metric_compartment_id_in_subtree = false
+  pending_duration                 = "PT5M"
+  repeat_notification_duration     = "PT2H"
+  resolution                       = "1m"
+
+  suppression {
+    time_suppress_from  = "2022-12-31T18:00:00.000Z"
+    time_suppress_until = "2022-12-31T19:00:00.000Z"
+    description = "Test alarms"
+  }
+}
+
 resource "oci_monitoring_alarm" "OIT_DEV_COMPUTES_MEMORY_CRITICAL_1211" {
   compartment_id        = var.compartment_id
   destinations          = [oci_ons_notification_topic.oci_topic.id]
